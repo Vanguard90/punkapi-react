ca50e6e307dc041b5421f55daac3d2e0649a3284
Vanguard90 <enesmil@gmail.com>
2019-01-12 20:41:24 +0100

    There is now an ExtendedBeer class that wraps around the api response and holds an internal state about the beer. Added babel plugin to make life easier. Created BeerList component to display a list of beers.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
 import punkApiService from '../service/punkapi-service';
+import BeerList from './BeerList';
+
+// Note that defining variables inside a class body is a stage 3 proposal, see: https://github.com/tc39/proposal-class-fields
+// but babel plugin "@babel/plugin-proposal-class-properties" is making it possible
+class ExtendedBeer {
+  isFavourite; // Is this beer a favourite beer?
+
+  beerObject; // Beer object itself that's been returned from the API call
+
+  constructor(beerObject, isFavourite) {
+    this.beerObject = beerObject;
+    this.isFavourite = isFavourite;
+  }
+}
 
 class App extends React.Component {
 
@@ -14,7 +28,7 @@ class App extends React.Component {
   getRandomBeerFn() {
     punkApiService.getRandomBeer().subscribe(randomBeer => {
       console.log(randomBeer);
-      this.setState(prevState => ({ allBeers: [...prevState.allBeers, randomBeer] }));
+      this.setState(prevState => ({ allBeers: [...prevState.allBeers, new ExtendedBeer(randomBeer)] }));
       console.log(this.state.allBeers);
       if (this.state.allBeers.length < 10) {
         // Make sure there are at least 10 random beers in the array
@@ -31,6 +45,12 @@ class App extends React.Component {
         <div className="content-area">
         <h1> PunkAPI Beer Service </h1>
           <button type="button" onClick={this.getRandomBeerFn}> BEER ME! </button>
+        </div>
+        <div className="beers-list">
+        <BeerList/>
+        </div>
+        <div className="beers-favourite-list">
+      <BeerList/>
         </div>
       </main>
     );
---
+++ src/components/BeerList.jsx
@@ -0,0 +1,18 @@
+
+ import React from 'react';
+
+class BeerList extends React.Component {
+  // A component that displays beers in a list format
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <p> Beer list component is alive! </p>
+    );
+  }
+}
+
+export default BeerList;
