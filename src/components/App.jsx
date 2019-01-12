15a9fb75067635982b4918307fec7c05e6e36860
Vanguard90 <enesmil@gmail.com>
2019-01-12 20:17:38 +0100

    API service call is now ready to get 10 random beers. Clicking on the button now displays 10 beers. Using CSS-Grid for the layout.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
+import punkApiService from '../service/punkapi-service';
 
 class App extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      allBeers: [],
+    };
+    this.getRandomBeerFn = this.getRandomBeerFn.bind(this);
+  }
+
+  getRandomBeerFn() {
+    punkApiService.getRandomBeer().subscribe(randomBeer => {
+      console.log(randomBeer);
+      this.setState(prevState => ({ allBeers: [...prevState.allBeers, randomBeer] }));
+      console.log(this.state.allBeers);
+      if (this.state.allBeers.length < 10) {
+        // Make sure there are at least 10 random beers in the array
+        this.getRandomBeerFn();
+      }
+    }, err => {
+      console.error(`Error when gettting a random beer! Err: ${err}`);
+    });
+  }
+
   render() {
     return (
-      <main>
-        <p className="sass-test">Sass-test</p>
+      <main className="main-container">
+        <div className="content-area">
+        <h1> PunkAPI Beer Service </h1>
+          <button type="button" onClick={this.getRandomBeerFn}> BEER ME! </button>
+        </div>
       </main>
     );
   }
