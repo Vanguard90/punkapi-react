b3155a103cf3f06e7ee9947c1cf197f518efbd82
Vanguard90 <enesmil@gmail.com>
2019-01-12 19:39:33 +0100

    Initial configuration complete to start working on the app. ESLint with appropriate configuration is being used as a linter and babel is being used as a compiler. Webpack works as expected both in dev and prod mode.

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <main>
+        <p className="sass-test">Sass-test</p>
+      </main>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './scss/style.scss';
+
+import App from './components/App';
+
+render(<App/>, document.querySelector('#root'));
